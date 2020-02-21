08e43f5571f7c599a6d58dbdf8869bcfd5ec28b2
Rafael Aranda Martins <skema1114@hotmail.com>
2020-02-21 09:07:09 -0300

    O inicio da correria

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(flutterSpech());
+
+class flutterSpech extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
